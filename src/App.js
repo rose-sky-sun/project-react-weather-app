c81f1a6b875a228d4dac239b0e19c354f1d19bf6
LunaFreya Dragonborn <rose.sky000801@gmail.com>
2024-09-07 19:16:46 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import SearchWeather from './SearchWeather';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Weather<img src={logo} className="App-logo" alt="logo" />Forecast</h1>
-      </header>
-      <SearchWeather />
-      <footer>
-        <p>This project is coded with 🩷 by 
-          <a href="https://github.com/rose-sky-sun"> LunaFreya Dragonborn </a> is 
-          <a href='https://github.com/rose-sky-sun/project-react-weather-app'> open-source on GitHub</a> and
-          <a href='https://weatherforecastbyluna.netlify.app'> hosted on Netlify.</a>
-        </p>
-      </footer>
+      <div className='container'>
+        <header className="App-header">
+          <h1>Weather<img src={logo} className="App-logo" alt="logo" />Forecast</h1>
+        </header>
+        <SearchWeather />
+        <footer>
+          <p>This project is coded with 🩷 by 
+            <a href="https://github.com/rose-sky-sun"> LunaFreya Dragonborn </a> is 
+            <a href='https://github.com/rose-sky-sun/project-react-weather-app'> open-source on GitHub</a> and
+            <a href='https://weatherforecastbyluna.netlify.app'> hosted on Netlify.</a>
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
